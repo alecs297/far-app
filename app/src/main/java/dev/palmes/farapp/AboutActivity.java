b18fb46446593a888ce3892258f1aac7a1306249
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-05-23 20:34:24 +0200

    "css" or something like this

---
+++ app/src/main/java/dev/palmes/farapp/AboutActivity.java
@@ -0,0 +1,33 @@
+package dev.palmes.farapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        WebView myWebView = findViewById(R.id.webview);
+        myWebView.loadUrl("https://find-a-room.palmes.dev/");
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
